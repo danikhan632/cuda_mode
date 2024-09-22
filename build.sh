e74bea238c66cc56df9bc480f413ba9fdca4c9f4
green <danikhan632@gmail.com>
2024-09-21 23:18:46 -0400

    matmul lowers2

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 clear
 # verilator --cc --exe --build -j 0 -Wall tb_sparse_coo_matmul.sv sparse_coo_matmul.sv --timing --binary --main --trace --CFLAGS "-std=c++20 " --top tb_sparse_coo_matmul 
-verilator --cc --exe --build -j 0 -Wall --timing --binary --main --trace --top tb_sparse_coo_matmul --CFLAGS "-std=c++20 -fcoroutines" tb_sparse_coo_matmul.sv sparse_coo_matmul.sv && \ 
-    ./obj_dir/Vtb_sparse_coo_matmul
+verilator --cc --exe --build -j 0 -Wall --timing --binary --main --trace --top tb_sparse_coo_matmul --CFLAGS "-std=c++20 -fcoroutines" tb_sparse_coo_matmul.sv sparse_coo_matmul.sv
